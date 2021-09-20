e93ecf08528180753e6336c456e240425c3a4f8b
bannu-code-star <nitinchiliveri007@gmail.com>
2021-09-20 19:46:42 +0530

    [Naveen] Added a evenodd code

---
+++ EvenOdd.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BasicCoreProgramming.cs
+{
+    class EvenOdd
+    {
+        //Program to check number is a even or odd
+        public static void NumberCheck()
+        {
+            //Take input from user
+            Console.WriteLine("Enter the number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("The given number is a even number");
+            }
+            else
+            {
+                Console.WriteLine("The given number is a odd number");
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            EvenOdd.NumberCheck();
+            Console.ReadLine();
+        }
+    }
+}
