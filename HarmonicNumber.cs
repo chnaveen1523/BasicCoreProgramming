ea3c785efc71295c423618699d6c779c914f624b
bannu-code-star <nitinchiliveri007@gmail.com>
2021-09-20 19:51:35 +0530

    [Naveen] Added a basicCode

---
+++ HarmonicNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BasicCoreProgramming.cs
+{
+    class HarmonicNumber
+    {
+        public static double sum = 0.0;
+
+        public static void HarmonicNumb()
+        {
+            Console.WriteLine("Enter the number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (double count = 1; count <= num; count++)
+            {
+                sum = sum + 1 / count;
+            }
+            Console.WriteLine("The Nth harmonic series is:" + sum);
+        }
+        static void Main(string[] args)
+        {
+            HarmonicNumber.HarmonicNumb();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LargestNumber.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace BasicCorePrograms_CS
+{/
+    class LargestNumber
+    {
+        //Program to find largest among three numbers
+        public static void Largest()
+        {
+            //taking input from user
+            Console.WriteLine("Enter first numbers:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second numbers:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter third numbers:");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            if (num1 > num2 && num1 > num3)
+            {
+                Console.WriteLine("{0} is the largest number", +num1);
+            }
+            else if (num2 > num1 && num2 > num3)
+            {
+                Console.WriteLine("{0} is the largest number", +num2);
+            }
+            else
+            {
+                Console.WriteLine("{0} is the largest number", +num3);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            LargestNumber.Largest();
+            Console.ReadLine();
+        }
+    }
+}
