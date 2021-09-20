e9e42943e8863fb3ef43f296a379f73b5182517b
bannu-code-star <nitinchiliveri007@gmail.com>
2021-09-20 19:57:54 +0530

    [Naveen] Added a code

---
+++ SwapNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BasicCoreProgramming.cs
+{
+    class SwapNumber
+    {
+        public static int temp;
+        //Program to swap two numbers
+        public static void Swapping()
+        {
+            //taking input from user
+            Console.WriteLine("Enter first number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Before swapping numbers are");
+            Console.WriteLine("num1=" + num1);
+            Console.WriteLine("num2=" + num2);
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+            Console.WriteLine("After swapping numbers are");
+            Console.WriteLine("num1=" + num1);
+            Console.WriteLine("num2=" + num2);
+        }
+
+        static void Main(string[] args)
+        {
+            SwapNumber.Swapping();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ VowelConsonant.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BasicCoreProgramming.cs
+{
+    class VowelConsonant
+    {
+        //Program to check alphabate is a vowel or consonant
+        public static void AlphabateCheck()
+        {
+            //Take input from user
+            Console.WriteLine("Enter a alphabate:");
+            char alpha = Convert.ToChar(Console.ReadLine());
+            //To check input is a vowel or consonant
+            if (alpha == 'a' || alpha == 'e' || alpha == 'i' || alpha == 'o' || alpha == 'u' || alpha == 'A' || alpha == 'E' || alpha == 'I' || alpha == 'O' || alpha == 'U')
+            {
+                Console.WriteLine("It is a Vowel");
+            }
+            else
+            {
+                Console.WriteLine("It is a Consonant");
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            VowelConsonant.AlphabateCheck();
+            Console.ReadLine();
+        }
+    }
+}
