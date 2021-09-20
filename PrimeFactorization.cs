43a7c7f079b5a1f294501eaa518c481eb603a351
bannu-code-star <nitinchiliveri007@gmail.com>
2021-09-20 19:55:58 +0530

    [Naveen] Added a BasicCoreProgramming code

---
+++ PrimeFactorization.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace BasicCoreProgramming.cs
+{
+    class PrimeFactorization
+    {
+        //Find prime factors of a number
+        public static void PrimeFactor()
+        {
+            //taking input from user
+            Console.WriteLine("Enter a number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            //local variable
+            int isPrime;
+            for (int count = 2; count <= num; count++)
+            {
+                if (num % count == 0)
+                {
+                    isPrime = 1;
+                    for (int j = 2; j <= count / 2; j++)
+                    {
+                        if (count % j == 0)
+                        {
+                            isPrime = 0;
+                            break;
+                        }
+                    }
+                    if (isPrime == 1)
+                    {
+                        Console.WriteLine("Prime factor is" + count);
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            PrimeFactorization.PrimeFactor();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Quotient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BasicCoreProgramming.cs
+{
+    class Quotient
+    {
+        public static int rem = 0, res = 0;
+
+        public static void FindQuotientRem()
+        {
+            //taking input of dividend and divisor from user
+            Console.WriteLine("Enter dividend=");
+            int div = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter divisor=");
+            int divisor = Convert.ToInt32(Console.ReadLine());
+            res = div / divisor;
+            rem = div % divisor;
+            Console.WriteLine("{0} and {1} is the quotient and remainder of {2}", +res, +rem, +div);
+        }
+
+        static void Main(string[] args)
+        {
+            Quotient.FindQuotientRem();
+            Console.ReadLine();
+        }
+    }
+}
