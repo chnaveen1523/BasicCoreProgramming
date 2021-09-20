5bbccbe5a6aba1e8b8434a4f8c1eabbf6ec34e74
bannu-code-star <nitinchiliveri007@gmail.com>
2021-09-20 19:53:46 +0530

    [Naveen] Added a BasicCoreProgramming

---
+++ LeapYear.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BasicCoreProgramming.cs
+{
+    class LeapYear
+    {
+        public static void leap()
+        {
+            Console.WriteLine("Enter a year:");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (((year % 400) == 0) || ((year % 4) == 0) && ((year % 100) != 0))
+            {
+                Console.WriteLine("{0} is a leap year", +year);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a leap year", +year);
+            }
+        }
+        static void Main(string[] args)
+        {
+            LeapYear.leap();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Powerof2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BasicCoreProgramming.cs
+{
+    class PowerOf2
+    {
+        public static void PowerCalci()
+        {
+            //taking input from user
+            Console.WriteLine("Enter the power of 2 till you want to display");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int count = 1; count <= num; count++)
+            {
+                //using Math.Pow method() to calulate power
+                //Pow method(2,count) means 2 to the power count
+                double pow = Math.Pow(2, count);
+                Console.WriteLine("power of 2 of {0} is {1}", +count, +pow);
+            }
+        }
+        static void Main(string[] args)
+        {
+            PowerOf2.PowerCalci();
+            Console.ReadLine();
+        }
+    }
+}
